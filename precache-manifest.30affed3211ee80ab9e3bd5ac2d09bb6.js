b9fa6098b79703ed78e9198076d59edeb7ee58c3
deepaksy <deepaksy28@gmail.com>
2020-08-07 17:13:37 +0530

    Updates

---
+++ precache-manifest.30affed3211ee80ab9e3bd5ac2d09bb6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b7cb53d9097f89eb7af0eb209934fd",
+    "url": "/React-based-projects/index.html"
+  },
+  {
+    "revision": "a433c015aead4d35c53b",
+    "url": "/React-based-projects/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "5e5e3600be565c9aa0e2",
+    "url": "/React-based-projects/static/css/main.2bc04692.chunk.css"
+  },
+  {
+    "revision": "a433c015aead4d35c53b",
+    "url": "/React-based-projects/static/js/2.9a0ce61a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-based-projects/static/js/2.9a0ce61a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5e3600be565c9aa0e2",
+    "url": "/React-based-projects/static/js/main.be934546.chunk.js"
+  },
+  {
+    "revision": "436a74bb920bc8db6f96",
+    "url": "/React-based-projects/static/js/runtime-main.331f4443.js"
+  },
+  {
+    "revision": "b041de9110673645a03a5a5224fd9dc3",
+    "url": "/React-based-projects/static/media/1.b041de91.jpg"
+  },
+  {
+    "revision": "83fa0f97876de62af2d6f78117906af7",
+    "url": "/React-based-projects/static/media/2.83fa0f97.jpg"
+  },
+  {
+    "revision": "18c175a219225fcc3f1aef05c92a9bf0",
+    "url": "/React-based-projects/static/media/3.18c175a2.png"
+  },
+  {
+    "revision": "4236359efd6afbfad379ff6d82cb70f1",
+    "url": "/React-based-projects/static/media/4.4236359e.png"
+  },
+  {
+    "revision": "2b41aec2e2525ed28a2205eb2bc92ee1",
+    "url": "/React-based-projects/static/media/5.2b41aec2.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-based-projects/static/media/logo.5d5d9eef.svg"
+  }
+]);
